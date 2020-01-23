412e8f091c4d53fc3a3a37bbe04866f319adb2df
GuilhermeLegramante <guilhermelegramante@gmail.com>
2020-01-23 14:54:50 -0300

    Teste grupo de rotas.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +9,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/teste', function () {
+        return view('welcome');
+    });
+});
